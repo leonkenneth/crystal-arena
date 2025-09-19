94937c769b40c88bb134cff47500028de95cb479
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-09-19 15:49:48 +0100

    Fix DamageZone display

--- client/src/components/Game/LifeAndDamageZone.tsx
+++ client/src/components/Game/LifeAndDamageZone.tsx
@@ -1,6 +1,6 @@
 import { DamageZoneState } from "@/types";
 import CollapsedZone from "./CollapsedZone";
-import { Container, Text } from "@chakra-ui/react";
+import { Container, HStack, Text } from "@chakra-ui/react";
 import Card from "./Card";
 import { CardState } from "@/types";
 
@@ -19,17 +19,17 @@ export default function LifeAndDamageZone({ damageZone, damageZoneName }: Props)
             >
             <Text color="fg.muted">Damage: {damageZone.cards.length}</Text>
             
-                <Container cursor="pointer">
-                {damageZone.cards.map((card : CardState) => (
+                <HStack cursor="pointer">
+                {damageZone.cards.map((card : CardState, index: number) => (
                     <Card key={card.cardId} card={card} containerProps={{
                         size: "xs",
                         isInteractable: true,
                         style:{
-                            transform: "scale(0.7)"
+                            transform: `scale(0.7) translateX(-${index * 2}rem)`
                         }
                     }} />
                 ))}
-                </Container>
+                </HStack>
             </CollapsedZone>
         </div>
     );
