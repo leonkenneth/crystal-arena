689efa300aeca129e8cd5f8c0bd1124d99dd7da9
Léo Goffic <leo.goffic@gmail.com>
2026-02-02 12:13:44 +0100

    Fix 23-025C Shelke triggering from opponents cards (#16)

--- engine/CrystalArena.Tests/FFTCGCards/Opus23/Opus23_025C_Shelke.cs
+++ engine/CrystalArena.Tests/FFTCGCards/Opus23/Opus23_025C_Shelke.cs
@@ -35,5 +35,33 @@
                     })
             );
         }
+
+        [Fact]
+        public void OpponentShelkeDoesNotTriggerMyShelke()
+        {
+            var myShelke = C("23-025C");
+            var opponentShelke = C("23-025C");
+            Hand(P1, myShelke);
+            Hand(P2, opponentShelke);
+
+            Exec(
+                At(Step.FirstMain, turn: 1)
+                    .Cast(myShelke)
+                    .Verify(() =>
+                    {
+                        True(P1.HasMana("{Z}".Parse()));
+                    }),
+                At(Step.FirstMain, turn: 2)
+                    .Cast(opponentShelke)
+                    .Verify(() =>
+                    {
+                        // P1's Shelke should not trigger from P2's Shelke entering
+                        False(P1.HasMana("{Z}{Z}".Parse()));
+                        True(P1.HasMana("{Z}".Parse()));
+                        // P2 should gain crystal from their own Shelke
+                        True(P2.HasMana("{Z}".Parse()));
+                    })
+            );
+        }
     }
 }
